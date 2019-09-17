3e6d25fe6c2db1e1300242d5da14bb07315d8600
Eddie Allard <eddie@theallardcompany.com>
2019-09-16 23:04:24 -0400

    animate images and create animate image functions

--- script.js
+++ script.js
@@ -5,22 +5,23 @@ var topics = ["Sports", "News", "Movies", "Cars", "Techy"];
 function showButtons() {
     $(".buttonsforthepage").empty();
     for (var i = 0; i < topics.length; i++) {
-        var gifbuttons = $("<button>");
-        gifbuttons.attr("type", "button");
-        gifbuttons.text(topics[i]);
-        $(".buttonsforthepage").append(gifbuttons);
+        var gifButtons = $("<button>");
+        gifButtons.attr("data-name", topics[i]);
+        gifButtons.addClass("button")
+        gifButtons.text(topics[i]);
+        $(".buttonsforthepage").append(gifButtons);
     }
 }
-showButtons();
 
 
-$("button").on("click", function () {
+
+$(document).on("click", ".button", function () {
     // In this case, the "this" keyword refers to the button that was clicked
-    var topic = $(this).text().trim();
+    var topics = $(this).attr("data-name");
 
     // PLACE NON ANIMATED 10 GIF IMAGES ON PAGE WHEN USER CLICKS ON A BUTTON
     var queryURL = "https://api.giphy.com/v1/gifs/search?q=" +
-        topic + "&api_key=KtSDmHGXP9azrYkXiBni2G0wX2dVhoCN&limit=10";
+        topics + "&api_key=KtSDmHGXP9azrYkXiBni2G0wX2dVhoCN&limit=10";
 
 
     // Performing our AJAX GET request
@@ -28,39 +29,44 @@ $("button").on("click", function () {
         url: queryURL,
         method: "GET"
     }).then(function (response) {
-        console.log(response);
-    });
-    // After the data comes back from the API
+
+            var results = response.data
+
+            for (var i = 0; i < results.length; i++) {
+
+                // DISPLAY GIF RATINGS UNDER PICTURES
+                var gifDiv = $("<div>");
+                var pTag = $("<p>");
+                var image = $("<img>");
+
+                var fixedPic = results[i].images.fixed_height_still.url
+                var animatePic = results[i].images.fixed_height.url
+
+                var ratePic = results[i].rating
+
+                image.attr("src", fixedPic)
+                image.attr("data-state", "still");
+                image.attr("data-animate", animatePic);
+                image.attr("data-still", fixedPic);
+
+                //append img to gifDiv  
+                gifDiv.append(image);
+                //append gifDiv to your HTML element to hold your gifs
+
+                //create a var for <p>                  
+                //set paragraph text to the rating. (HINT: You named that shit "Rating" like 5 lines up)                                  
+                //append the <p> to gifDiv too.
 
 
-    // FOR LOOP TO APPEND A BUTTON FOR EACH STRING
-    for (var i = 0; i < 9; i++) {
+                // CREATE BUTTONS IN THE HTML
+                $(".gifs-appear-here").prepend(gifDiv);
 
-    // DISPLAY GIF RATINGS UNDER PICTURES
-        var gifDiv = $("<div>");
-        var pTag = $("<p>");
-        var image = $("<img>");
-
-        var fixedPic = response.data[i].images.fixed_height_still.url
-        var animatePic = response.data[i].images.fixed_height.url
-
-        var ratePic = response.data[i].rating
-
-        image.attr("src", fixedPic)
-        image.attr("data-state", "still", fixedPic)
-
-
-
-
-        
-
-
-
-        // CREATE BUTTONS IN THE HTML
-        $(".gifs-appear-here").append(button);
+            };
+            });
+    
 
         // STILL GIPH IMAGE ANIMATES WHEN CLICKED ON, GIF IMAGE STOPPED WHEN PRESSED
-        $(".gif").on("click", function () {
+        $(document).on("click", "img", function () {
             var state = $(this).attr("data-state");
             if (state === "still") {
                 $(this).attr("src", $(this).attr("data-animate"));
@@ -72,9 +78,5 @@ $("button").on("click", function () {
         });
 
 
-        // ADD FORM TO PAGE THAT TAKES VALUE FROM INPUT BOX AND ADDS TO TOPICS ARRAY
-
-        // MAKE A FUNCTION THAT TAKES EACH TOPIC IN ARRAY AND REMAKES THE BUTTONS ON PAGE
-
-    }
 });
+showButtons();
